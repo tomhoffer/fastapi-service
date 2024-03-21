0f7e1d5cf2650152af4084e4c3768580c75b3a48
Tomas Hoffer <tomas.hoffer@gmail.com>
2024-03-21 21:45:06 +0100

    Asyncio rewrite: Adjust unit tests

--- test/unit/test_user_get.py
+++ test/unit/test_user_get.py
@@ -1,24 +1,31 @@
-from starlette.testclient import TestClient
+import asyncio
+
+import pytest
+from httpx import AsyncClient
 from src.main import app
 
-client = TestClient(app)
+client = AsyncClient(app=app, base_url="http://test")
 
 
-def test_user_get_invalid_email(mocker):
+@pytest.mark.asyncio
+async def test_user_get_invalid_email(mocker):
+    future = asyncio.Future()
+    future.set_result([])
     mock_db = mocker.Mock()
-    mock_db.get_record_by_email.return_value = []
+    mock_db.get_record_by_email.return_value = future
     mocker.patch("src.main.records_db", new=mock_db)
     # API returns 404 if user with given email does not exist
-    response = client.get("/user?email=some_email@gmail.com")
+    response = await client.get("/user?email=some_email@gmail.com")
     assert response.status_code == 404
 
 
-def test_valid_user_get(mocker):
+@pytest.mark.asyncio
+async def test_valid_user_get(mocker):
     # API returns the text for given valid email address
     mocker.patch(
         "src.db.RecordsDbRepository.get_record_by_email", return_value=["Result"]
     )
-    response = client.get("/user?email=some_email@gmail.com")
+    response = await client.get("/user?email=some_email@gmail.com")
     body = response.json()
     assert response.status_code == 200
     assert body == ["Result"]
